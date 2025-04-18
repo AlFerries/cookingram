aad67c1465e7946d608cb2614c830e486a4232e0
Alex Ferries <alex@alexferries.co.uk>
2025-04-18 17:57:35 +0100

    fixed parsing ingredients for recipe

--- src/libs/mealDb.js
+++ src/libs/mealDb.js
@@ -5,12 +5,13 @@ export function parseMealDBRecipe (data) {
 
   const ingredients = [];
   for (let i = 0; i <= 20; i++) {
-    const ingredient = data[`strIngredient${i}`];
-    const measure = data[`strMeasure${i}`];
+    const ingredient = data[0][`strIngredient${i}`];
+    const measure = data[0][`strMeasure${i}`];
     if (ingredient) {
       ingredients.push({item: ingredient, measure});
     }
   }
+
 
   return {
     id: idMeal, 
