3cf90425c35d373335e3d3e4c2f43e7cae97cd33
Alex Ferries <aleksander.shishkov@gmail.com>
2025-04-19 16:47:04 +0100

    cosmetic fixes for burger menu

--- src/components/nav-menu.js
+++ src/components/nav-menu.js
@@ -1,15 +1,34 @@
-'use client'
+'use client';
 
-import Link from "next/link"
-import { useState } from "react"
-import { Menu, X } from "lucide-react"
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Menu, X } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 // TODO: show "login" only for unauthorized only, and show "logout" for authorized
-// TODO: do not show "login" on login page
 
 export default function NavMenu() {
 
-  const [menuOpen, setMenuOpen] = useState(false)
+  const [menuOpen, setMenuOpen] = useState(false);
+  const loginPage = usePathname() === '/login';
+
+  {/** block scroll when menu is opened */}
+  useEffect(() => {
+    document.body.style.overflow = menuOpen ? 'hidden' : '';
+    return () => {
+      document.body.style.overflow = '';
+    }
+  }, [menuOpen]);
+
+  {/** close menu if go back/forward in browser */}
+  useEffect(() => {
+    const handlePop = () => {
+      setMenuOpen(prev => false)
+    }
+    window.addEventListener("popstate", handlePop)
+
+    return () => document.removeEventListener("popstate", handlePop)
+  },[]);
 
   const navItems = [
     { label: 'desserts' ,href:'/recipes/dessert' },
@@ -18,10 +37,11 @@ export default function NavMenu() {
     { label: 'side' ,href:'/recipes/side' },
     { label: 'starter' ,href:'/recipes/starter' },
     { label: 'seafood' ,href:'/recipes/seafood' },
-  ]
+  ];
 
   return (
     <>
+    {/** desktop menu */}
     <ul className="hidden md:flex gap-6 w-full">
       {navItems.map(({label, href}, index) => (
         <li key={index}>
@@ -29,28 +49,35 @@ export default function NavMenu() {
         </li>
       ))}
       <li className="ml-auto">
-        <Link href="/login">login</Link>
+        {!loginPage && <Link href="/login">login</Link>}
       </li>
     </ul>
+
+    {/** mobile burger menu */}
     <button onClick={()=>{setMenuOpen(prev => !prev)}} className="md:hidden text-2xl z-20 relative ml-auto">
       {menuOpen ? <X /> : <Menu />}
     </button>
-    {menuOpen && (
-      <div className="absolute top-full left-0 w-full bg-white z-40 shadow-md py-4">
-        <ul className="flex flex-col items-center gap-4">
-          {navItems.map(({label, href}, index) => (
-            <li key={index}>
-              <Link href={href} onClick={() => setMenuOpen(prev => false)}>
-                {label}
-              </Link>
+      <>
+        {/** backdrop for mobile menu */}
+        <div className={`fixed inset-0 bg-black transition-opacity duration-300 z-30 ${menuOpen ? 'opacity-50 pointer-events-auto' : 'opacity-0 pointer-events-none'} `}
+            onClick={() => setMenuOpen(prev => false)} 
+        />
+        {/** mobile menu list */}
+        <div className={`fixed top-16 left-0 w-full bg-white z-40 shadow-md py-4 transition-all transform ${menuOpen ? 'duration-300 opacity-100 translate-y-0' : 'duration-0 opacity-0 -translate-y-4 pointer-events-none'}`}>
+          <ul className="flex flex-col items-center gap-4">
+            {navItems.map(({label, href}, index) => (
+              <li key={index}>
+                <Link href={href} onClick={() => setMenuOpen(prev => false)}>
+                  {label}
+                </Link>
+              </li>
+            ))}
+            <li>
+              {!loginPage && <Link href="/login" onClick={() => setMenuOpen(prev => false)}>login</Link>}
             </li>
-          ))}
-          <li>
-            <Link href="/login" onClick={() => setMenuOpen(prev => false)}>login</Link>
-          </li>
-        </ul>
-      </div>
-    )}
+          </ul>
+        </div>
+      </>
     </>
-  )
+  );
 }
