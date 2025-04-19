004c63a64816de6c5c4c53cb43e085591123ff94
Alex Ferries <alex@alexferries.co.uk>
2025-04-19 01:40:25 +0100

    substitute current menu to a client menu component

--- src/components/header.js
+++ src/components/header.js
@@ -1,20 +1,13 @@
 import { Lily_Script_One } from "next/font/google";
 import Link from "next/link";
+import NavMenu from "./nav-menu";
 
 const lilyFont = Lily_Script_One({subsets:['latin'], weight:['400']});
 
 const Header = () => (
   <nav className="py-4 px-8 flex items-baseline relative">
     <h1 className={`${lilyFont.className} antialiased text-3xl font-extrabold pr-16`}><Link href="/">Cookingram</Link></h1>
-    <ul className="flex gap-4">
-      <li><Link href="/recipes/dessert">desserts</Link></li>
-      <li><Link href="/recipes/breakfast">breakfast</Link></li>
-      <li><Link href="/recipes/pasta">pasta</Link></li>
-      <li><Link href="/recipes/side">side</Link></li>
-      <li><Link href="/recipes/starter">starter</Link></li>
-      <li><Link href="/recipes/seafood">seafood</Link></li>
-    </ul>
-    <button className="ml-auto"><Link href="/login">login</Link></button>
+      <NavMenu />
   </nav>
 );
 
