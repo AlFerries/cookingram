ed007173993449f42076f8c46e87be6516088588
Alex Ferries <alex@alexferries.co.uk>
2025-04-19 02:12:29 +0100

    added ideas in TODO

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,7 @@
-
+// TODO:  Build main page
+//        - There shoud be recipes from every category
+//        - Ideally Hero with desciption what this site is about
+//        - Ideally "most favourited", when favourite functional will be made
 
 export default function Home() {
   return (
